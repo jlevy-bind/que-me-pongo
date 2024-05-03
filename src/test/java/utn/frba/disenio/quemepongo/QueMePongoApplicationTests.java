de98917e993935ab6d12624123689bb75124bce9
Julian Ignacio LEVY <jlevy@bancoindustrial.com.ar>
2024-05-03 11:31:20 -0300

    Initial commit

---
+++ src/test/java/utn/frba/disenio/quemepongo/QueMePongoApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.frba.disenio.quemepongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueMePongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
